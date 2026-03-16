a1810118802c18621daa67e4750b36b8dce1304b
Jonghwa <whdghk3471@gmail.com>
2026-03-16 18:28:23 +0900

    #7.9 File Download

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractor import extract_wanted
+from file import save_to_file
 
 app = Flask("Job Scrapper")
 
@@ -14,12 +15,25 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
     if keyword in db:
         wanted_jobs = db[keyword]
     else:
         wanted_jobs = extract_wanted(keyword)
         db[keyword] = wanted_jobs
     return render_template("search.html", keyword=keyword, jobs=wanted_jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run(debug=True)
