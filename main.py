b5628099b7e1ddf4eda999525f3ddff92332af28
Jonghwa <whdghk3471@gmail.com>
2026-03-26 15:34:21 +0900

    add: static pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from extractor import extract_berlin, extract_web3, extract_weworkremotely
 from flask_frozen import Freezer
 
@@ -6,31 +6,38 @@ app = Flask("Job Scrapper")
 freezer = Freezer(app)
 
 KEYWORDS = ["python", "javascript", "typescript", "rust"]
-db = None
+db = {}
 
 
-def extract_all_jobs():
-    all_jobs = []
-
-    for keyword in KEYWORDS:
-        berlin_jobs = extract_berlin(keyword)
-        web3_jobs = extract_web3(keyword)
-        weworkremotely_jobs = extract_weworkremotely(keyword)
-        all_jobs.extend(berlin_jobs)
-        all_jobs.extend(web3_jobs)
-        all_jobs.extend(weworkremotely_jobs)
-
-    return all_jobs
+def extract_jobs_by_keyword(keyword):
+    berlin_jobs = extract_berlin(keyword)
+    web3_jobs = extract_web3(keyword)
+    weworkremotely_jobs = extract_weworkremotely(keyword)
+    return [*berlin_jobs, *web3_jobs, *weworkremotely_jobs]
 
 
 @app.route("/")
+def home():
+    return render_template("home.html", keywords=KEYWORDS)
+
+
+@app.route("/search/<keyword>/")
+def search(keyword):
+    keyword = keyword.lower()
+
+    if keyword not in KEYWORDS:
+        return redirect("/")
+
+    if keyword not in db:
+        db[keyword] = extract_jobs_by_keyword(keyword)
+
+    return render_template("search.html", keyword=keyword, jobs=db[keyword])
+
+
+@freezer.register_generator
 def search():
-    global db
-
-    if db is None:
-        db = extract_all_jobs()
-
-    return render_template("search.html", keyword="All Jobs", jobs=db)
+    for keyword in KEYWORDS:
+        yield {"keyword": keyword}
 
 
 if __name__ == "__main__":
