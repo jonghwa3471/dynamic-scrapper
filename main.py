769d3db73b09b7375e00e57be16566edbda56877
Jonghwa <whdghk3471@gmail.com>
2026-03-26 13:59:13 +0900

    build before deploy

--- main.py
+++ main.py
@@ -1,59 +1,37 @@
-from flask import Flask, render_template, request, redirect, send_file
-from extractor import (
-    extract_wanted,
-    extract_berlin,
-    extract_web3,
-    extract_weworkremotely,
-)
-from file import save_to_file
+from flask import Flask, render_template
+from extractor import extract_berlin, extract_web3, extract_weworkremotely
 from flask_frozen import Freezer
 
 app = Flask("Job Scrapper")
 freezer = Freezer(app)
 
-db = {}
-PREBUILD_KEYWORDS = ["python", "javascript", "typescript", "rust"]
+KEYWORDS = ["python", "javascript", "typescript", "rust"]
+db = None
 
 
-@app.route("/")
-def home():
-    return render_template("home.html", name="nico")
+def extract_all_jobs():
+    all_jobs = []
 
-
-@app.route("/search/<keyword>/")
-def search(keyword):
-    keyword = request.args.get("keyword")
-    if keyword == None or keyword == "":
-        return redirect("/")
-    if keyword in db:
-        all_jobs = db[keyword]
-    else:
+    for keyword in KEYWORDS:
         berlin_jobs = extract_berlin(keyword)
         web3_jobs = extract_web3(keyword)
         weworkremotely_jobs = extract_weworkremotely(keyword)
-        all_jobs = [*berlin_jobs, *web3_jobs, *weworkremotely_jobs]
-        db[keyword] = all_jobs
-    return render_template("search.html", keyword=keyword, jobs=all_jobs)
+        all_jobs.extend(berlin_jobs)
+        all_jobs.extend(web3_jobs)
+        all_jobs.extend(weworkremotely_jobs)
+
+    return all_jobs
 
 
-@freezer.register_generator
+@app.route("/")
 def search():
-    for keyword in PREBUILD_KEYWORDS:
-        yield {"keyword": keyword}
+    global db
+
+    if db is None:
+        db = extract_all_jobs()
+
+    return render_template("search.html", keyword="All Jobs", jobs=db)
 
 
 if __name__ == "__main__":
     freezer.freeze()
-
-""" @app.route("/export")
-def export():
-    keyword = request.args.get("keyword")
-    if keyword == None or keyword == "":
-        return redirect("/")
-    if keyword not in db:
-        return redirect(f"/search?keyword={keyword}")
-    save_to_file(keyword, db[keyword])
-    return send_file(f"{keyword}.csv", as_attachment=True)
-
-
-app.run(debug=True) """
